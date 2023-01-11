80d55db0b18092bdb4f8dbf43eb2e09bdab7a648
Mohammed Salah <mohammed.salah@agreena.com>
2023-01-11 14:37:44 +0200

    adding node to this script

--- update-wiki.sh
+++ update-wiki.sh
@@ -1,32 +1,35 @@
 #!/bin/sh
+
 set -eu
+WIKI_DIR='wiki'
 
 TEMP_REPO_DIR="wiki_action_$GITHUB_REPOSITORY$GITHUB_SHA"
-TEMP_WIKI_DIR="temp_wiki_$GITHUB_SHA"
+TEMP_WIKI_DIR="wiki_action_$GITHUB_REPOSITORY$WIKI_DIR$GITHUB_SHA"
 
-if [ -z "$WIKI_DIR" ]; then
-    echo "Wiki location is not specified, using default wiki/"
-    WIKI_DIR='wiki'
-fi
 
 if [ -z "$GH_TOKEN" ]; then
     echo "Token is not specified"
     exit 1
 fi
 
-if which node > /dev/null then
-    echo "node is installed, skipping..."
+if !(which node > /dev/null); then
+    echo "node is not installed"
     exit 1
 fi
 
 #Clone repo
 echo "Cloning repo https://github.com/$GITHUB_REPOSITORY"
 git clone "https://$GITHUB_ACTOR:$GH_TOKEN@github.com/$GITHUB_REPOSITORY" "$TEMP_REPO_DIR"
-
+cd "$TEMP_REPO_DIR"
 #Clone wiki repo
 echo "Cloning wiki repo https://github.com/$GITHUB_REPOSITORY.wiki.git"
-cd "$TEMP_REPO_DIR"
-git clone "https://$GITHUB_ACTOR:$GH_TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git" "$TEMP_WIKI_DIR"
+git clone "https://$GITHUB_ACTOR:$GH_TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git" "temp_$WIKI_DIR"
+
+#build Wiki
+npm ci
+npm run docs
+npm run html2md
+
 
 #Get commit details
 author=`git log -1 --format="%an"`
@@ -34,7 +37,7 @@ email=`git log -1 --format="%ae"`
 message=`git log -1 --format="%s"`
 
 echo "Copying edited wiki"
-cp -R "$TEMP_WIKI_DIR/.git" "$WIKI_DIR/"
+cp -R "temp_$WIKI_DIR/.git" "$WIKI_DIR/"
 
 echo "Checking if wiki has changes"
 cd "$WIKI_DIR"
